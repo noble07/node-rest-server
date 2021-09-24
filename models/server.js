14f500513428a7c654dcb36f4ad512ce31fefd9f
Juan Camilo <juancmz98@gmail.com>
2021-09-24 00:26:17 -0500

    Added: CORS

--- models/server.js
+++ models/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const cors = require('cors')
 class Server {
   constructor() {
     this.app = express()
@@ -12,13 +12,42 @@ class Server {
   }
 
   middlewares() {
+    // CORS
+    this.app.use(cors())
+
     // Directorio público
     this.app.use(express.static('public'))
   }
 
   routes() {
     this.app.get('/api', (req, res) => {
-      res.send('Hellow World')
+      res.json({
+        msg: 'get API'
+      })
+    })
+
+    this.app.put('/api', (req, res) => {
+      res.json({
+        msg: 'put API'
+      })
+    })
+
+    this.app.post('/api', (req, res) => {
+      res.status(201).json({
+        msg: 'post API'
+      })
+    })
+
+    this.app.delete('/api', (req, res) => {
+      res.json({
+        msg: 'delete API'
+      })
+    })
+
+    this.app.patch('/api', (req, res) => {
+      res.json({
+        msg: 'patch API'
+      })
     })
   }
 
