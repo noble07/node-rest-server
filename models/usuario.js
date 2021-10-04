db9c9d646bf2f4ed6ad90258ea9ea20937d6c3ef
Juan Camilo <juancmz98@gmail.com>
2021-10-03 21:24:53 -0500

    Added: User Model

---
+++ models/usuario.js
@@ -0,0 +1,33 @@
+const {Schema, model} = require('mongoose')
+
+const userSchema = Schema({
+  name: {
+    type: String,
+    required: [true, 'El nombre es obligatorio']
+  },
+  email: {
+    type: String,
+    required: [true, 'El correo es obligatorio'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'La contraseña es obligatoria']
+  },
+  img: String,
+  role: {
+    type: String,
+    required: true,
+    enum: ['ADMIN_ROLE', 'USER_ROLE']
+  },
+  state: {
+    type: Boolean,
+    default: true
+  },
+  google: {
+    type: Boolean,
+    default: false
+  }
+})
+ 
+module.exports = model('User', userSchema)
