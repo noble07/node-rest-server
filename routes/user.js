951c9964da8a28a546632164f9a075aa4ec2ec88
Juan Camilo <juancmz98@gmail.com>
2021-10-05 00:19:10 -0500

    Added: Role model and role validation on route

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const { body } = require('express-validator')
+const Role = require('../models/role')
 
 const { validateFields } = require('../middlewares/validateFields')
 
@@ -19,7 +20,11 @@ router.post('/', [
   body('name', 'El nombre es obligatorio').not().isEmpty(),
   body('password', 'El password debe de ser más de 6 letras').isLength({ min: 6 }),
   body('email', 'El correo no es válido').isEmail(),
-  body('role', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+  // body('role', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+  body('role').custom(async(role = '') => {
+    const roleExists = await Role.findOne({ role })
+    if(!roleExists) throw new Error(`El rol ${role} no está registrado en la BBDD`)
+  }),
   validateFields
 ], userPost)
 
