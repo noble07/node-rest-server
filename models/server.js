27ee9e10daca02cb653d562ee549f9a1b4669d06
Juan Camilo <juancmz98@gmail.com>
2021-11-23 01:25:21 -0500

    Added: JWT Authentication

--- models/server.js
+++ models/server.js
@@ -7,6 +7,7 @@ class Server {
     this.app = express()
     this.port = process.env.PORT
     this.userPath = '/api/user'
+    this.authPath = '/api/auth'
 
     // Conectar a BD
     this.connectDB()
@@ -33,6 +34,7 @@ class Server {
   }
 
   routes() {
+    this.app.use(this.authPath, require('../routes/auth'))
     this.app.use(this.userPath, require('../routes/user'))
   }
 
